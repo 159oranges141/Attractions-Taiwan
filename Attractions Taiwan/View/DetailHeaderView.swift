f41a8ef151f2dc36280fc83f5c81145fab1a020f
159oranges141 <zxc1595141@gmail.com>
2021-12-13 16:54:31 +0800

    1213 too hard :(

---
+++ Attractions Taiwan/View/DetailHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailHeaderView.swift
+//  Attractions Taiwan
+//
+//  Created by NDHU_CSIE on 2021/12/13.
+//
+
+import UIKit
+
+class DetailHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Attractions Taiwan/View/RoundedTextField.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedTextField.swift
+//  Attractions Taiwan
+//
+//  Created by NDHU_CSIE on 2021/12/13.
+//
+
+import UIKit
+
+class RoundedTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
